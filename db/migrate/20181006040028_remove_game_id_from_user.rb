43261d9a318547dd59cdff3d22859dd6026658fa
Jordan Slominski <syanara17@gmail.com>
2018-10-07 22:25:23 -0700

    resolve migrate issue

--- db/migrate/20181006040028_remove_game_id_from_user.rb
+++ db/migrate/20181006040028_remove_game_id_from_user.rb
@@ -1,6 +1,6 @@
 class RemoveGameIdFromUser < ActiveRecord::Migration[5.1]
   def change
     remove_column :users, :game_id, :string
-    remove_column :users, :integer, :string
+    # remove_column :users, :integer, :string
   end
 end
