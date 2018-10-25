397f9e662267dbf49d8ff1bf459e4f6ba21cd4a7
Jordan Slominski <jslominski@novillus.com>
2018-10-25 13:37:56 -0700

    add total plays function

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,8 @@ class Player < ApplicationRecord
     belongs_to :game
 
     validates_uniqueness_of :jersey_number, scope: :game_id
+
+    def total_plays
+        self.quarter_one_plays.to_i + self.quarter_two_plays.to_i + self.quarter_three_plays.to_i + self.quarter_four_plays.to_i + self.overtime_plays.to_i
+    end
 end
