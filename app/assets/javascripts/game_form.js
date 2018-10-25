01299051cc05b4aded484d3b87ce9f098cb214d9
Jordan Slominski <jslominski@novillus.com>
2018-10-25 13:38:54 -0700

    fix JS

--- app/assets/javascripts/game_form.js
+++ app/assets/javascripts/game_form.js
@@ -81,14 +81,17 @@ function player_activation(button){
             current_quarter = index + 1;
         }
     });
+    
     $("input[id$='" + quarter_to_word(current_quarter) + "_plays']").each(function(index, play_counter){
-        if(play_counter.value == '' || play_counter.value == 0)
-        {
-            play_counter.value = 1;
-        } else {
-            play_counter.value ++;
-        }
-        
+        if(play_counter.parentNode.parentNode.classList.contains("tr-grey") == false){
+            if(play_counter.value == '' || play_counter.value == 0)
+            {
+                play_counter.value = 1;
+            } else {
+                play_counter.value ++;
+            }
+            play_counter.parentNode.nextSibling.nextSibling.getElementsByTagName('span')[0].innerText ++;
+        };
     });
   }
   
